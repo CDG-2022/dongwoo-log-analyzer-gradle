fa0f86d9cbe65884991439e9b2ce88b77cde2241
wellbeing-dough <kdw990202@naver.com>
2022-07-27 17:14:12 +0900

    class생성, 상태코드 테스트 완료

---
+++ src/main/java/LogAnalyze.java
@@ -0,0 +1,49 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.apache.commons.lang3.StringUtils.substringsBetween;
+
+public class LogAnalyze {
+
+    private HashMap<String, Integer> apiKey = new HashMap<>();
+    private HashMap<String, Integer> stateCode = new HashMap<>();
+    private HashMap<String, Integer> serviceId = new HashMap<>();
+    private HashMap<String, Integer> Time = new HashMap<>();
+    private HashMap<String, Integer> browser = new HashMap<>();
+
+    private String [] string;
+
+    public void divide(BufferedReader log) throws IOException {
+        string = substringsBetween(log.readLine(), "[", "]");
+    }
+
+    public void sampleApiKey() {
+        
+    }
+
+    public void sampleStateCode() {
+        stateCode.put(string[0], stateCode.getOrDefault(string[0], 0) + 1);
+    }
+
+    public void sampleServiceId() {
+
+    }
+
+    public void sampleTime() {
+
+    }
+
+    public void browser() {
+
+    }
+
+    public void Test() {
+        stateCode.forEach((key, value) -> {
+            System.out.println(key + " : " + value);
+        });
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello");
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        LogAnalyze logAnalyze = new LogAnalyze();
+
+        try {
+            FileReader fr = new FileReader("C://input.log");
+            BufferedReader br = new BufferedReader(fr);
+            while((br.readLine()) != null){
+                logAnalyze.divide(br);
+                logAnalyze.sampleStateCode();
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("파일을 찾지 못했습니다");
+        }
+        logAnalyze.Test();
     }
 }
