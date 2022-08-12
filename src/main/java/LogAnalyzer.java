618e7573f92bce777dfe183202a439f24749c9c2
wellbeing-dough <kdw990202@naver.com>
2022-08-13 05:52:59 +0900

    create LogSampler
    LogDivider에서 4개로 쪼갠 로그에 각 하나하나에서 apikey, stateCode, serviceId,  Time, browser 들을 추출하는 class

--- src/main/java/LogAnalyzer.java
+++ src/main/java/LogAnalyzer.java
@@ -1,17 +1,30 @@
+import lombok.Getter;
+
 import java.io.IOException;
 
 public class LogAnalyzer {
 
-    private LogFileReader logFileReader;
+    @Getter
+    private String lineOfLog;
+    private final LogFileReader logFileReader;
+    private final LogMap logMap = new LogMap();
+    private final LogDivider logDivider = new LogDivider();
+    private LogSampler logSampler;
+
 
     public LogAnalyzer(LogFileReader logFileReader) {
         this.logFileReader = logFileReader;
     }
 
     public void start() throws IOException {
-        while(logFileReader.read() != null) {
-            
-
+        while(true) {
+            lineOfLog = logFileReader.read();
+            if(lineOfLog == null) {
+                break;
+            }
+            logDivider.divide(lineOfLog);
+            logSampler.samplingLog(logDivider.getDividedResult());
+            logSampler()
         }
     }
 }
--- src/main/java/LogSampler.java
+++ src/main/java/LogSampler.java
@@ -1,5 +1,13 @@
+import lombok.Data;
+
+@Data
 public class LogSampler {
 
+    private String apiKey;
+    private String stateCode;
+    private String serviceId;
+    private String Time;
+    private String browser;
 
 
 }
