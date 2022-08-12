0802758ff40834a45cd3edf5ad9fbc3dfe644d83
wellbeing-dough <kdw990202@naver.com>
2022-08-13 05:06:04 +0900

    DividedLog->LogDivider
    java naming convention

--- src/main/java/LogDivider.java
+++ src/main/java/LogDivider.java
@@ -1,10 +1,7 @@
 import lombok.Data;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
 
 @Data
-public class DividedLog {
+public class LogDivider {
     private String apiKey;
     private String stateCode;
     private String serviceId;
